2cde45627cb8ea10473f481ee5deccce8bc92303
suhas-ux <57421700+suhas-ux@users.noreply.github.com>
2021-06-05 22:58:42 -0500

    Update autoarrest.lua

--- autoarrest.lua
+++ autoarrest.lua
@@ -1,1 +1,1 @@
-return game.Players.LocalPlayer:Kick("Jailbreaded has been patched, thank you for choosing PayPal v5, oh yeah and remember that skullkid is a pro gamer :pog:");
+return game.Players.LocalPlayer:Kick("Jailbreaded has been patched, thank you for choosing PayPal v5, oh yeah and remember that skullkid is a pro gamer :pog: --The Proxide");
